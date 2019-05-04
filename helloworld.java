5abb4b44a8beb792c28783bb0277628b7ec89cf4
root <root@ip-172-31-30-154.ap-south-1.compute.internal>
2019-05-04 19:34:28 +0000

    AD02

--- helloworld.java
+++ helloworld.java
@@ -1,7 +1,7 @@
 /*Sample Java Code*/
-class HelloWorld 
+public class HelloWorld 
 {
-    public static void main(String args[]) 
+    public static void main(String[] args) 
     { 
         System.out.println("Hello, World"); 
     } 
